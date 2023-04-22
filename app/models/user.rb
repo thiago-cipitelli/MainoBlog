c6b3e10ad04078dde4aa1eaa46f8d4074c97be3a
cobaia2 <cobaia2@RubyDev.myguest.virtualbox.org>
2023-04-22 13:31:33 -0300

    feat: user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :email, presence: true, format: { with: /[a-z0-9.]+@([a-z0-9]+\.)+[a-z]{2,4}/, message: "must be a valid email"} 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema[7.0].define(version: 2023_04_22_154940) do
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "email"
+    t.string "email", null: false
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
