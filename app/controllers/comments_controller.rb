443a3b293a2cee6043a205772332b3e17ccce85c
cobaia2 <cobaia2@RubyDev.myguest.virtualbox.org>
2023-04-25 20:22:08 -0300

    fix: minor css adjusts in posts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,12 +15,22 @@ class CommentsController < ApplicationController
     
   end
 
+  def destroy
+    #@post = Post.find(params[:post_id])
+    #@comment = @post.comments.find
+  end
+
   private
     def comment_params
       if Current.user.present?
-        params.require(:comment).permit(:body).merge(commenter: Current.user.name)
+        params.require(:comment).permit(:body).merge(commenter: Current.user.name, user_id: Current.user.id)
       else
         params.require(:comment).permit(:body).merge(commenter: "Anonymous")
       end
     end
+
+    #def check_association
+    #  if Current.user.comments.find(params[:id])
+    #  
+    #end
 end
