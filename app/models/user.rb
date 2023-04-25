288628aa18091b190a4baf506eeffab3e9c782fc
cobaia2 <cobaia2@RubyDev.myguest.virtualbox.org>
2023-04-25 18:59:02 -0300

    fix: missing comment association with user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :posts
+  has_many :comments
 
   validates :email, presence: true, format: { with: /[a-z0-9.]+@([a-z0-9]+\.)+[a-z]{2,4}/, message: "must be a valid email"} 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_25_144111) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_25_212049) do
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
     t.text "body"
     t.integer "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
